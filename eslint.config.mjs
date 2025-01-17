f970e27b4a78ffa96fff423f051407e28df71a5a
ha0nk <9441719+4m2c4bl3@users.noreply.github.com>
2025-01-16 23:05:42 -0500

    initial linting/etc setup working

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { FlatCompat } from "@eslint/eslintrc";
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	eslintPluginPrettierRecommended,
+	{
+		...import('eslint-config-love'),
+		ignores: [
+			"**/node_modules/",
+			".git/",
+			"**/*.d.ts",
+			"**/*.{json}",
+		],
+		plugins: {
+			"@typescript-eslint": typescriptEslint,
+			'@stylistic': stylistic
+		},
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname
+			}
+		}
+		}];
